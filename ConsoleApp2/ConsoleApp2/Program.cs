f54fb029262f6576cbcd1f5498ceca1cfedda204
kdakdad <filatovvlad075@gmail.com>
2024-11-19 20:54:14 +0900

    metodi

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class MyClass
+    {
+        
+        public int AddIntegers(int a, int b)
+        {
+            return a + b;
+        }
+
+        
+        private double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            int sumInt = myClass.AddIntegers(5, 3); 
+            Console.WriteLine("Сумма целых чисел: " + sumInt);
+
+            double sumDouble = myClass.AddDoubles(5.5, 3.2); 
+            Console.WriteLine("Сумма вещественных чисел: " + sumDouble);
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class User
+{
+    public string Username { get; set; } // Имя пользователя
+    public string Email { get; set; } // Email
+    private string Password { get; set; } // Пароль (приватный для безопасности)
+    public string Address { get; set; } // Адрес
+    public int Age { get; set; }
+
+    public User(string username, string email, string password, string address, int age)
+    {
+        this.Username = username;
+        this.Email = email;
+        this.Password = password;
+        this.Address = address;
+        this.Age = age;
+    }
+
+    // Геттер для пароля (безопасный доступ)
+    public string GetPassword()
+    {
+        return this.Password;
+    }
+}
